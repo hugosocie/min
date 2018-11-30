06796bd6de4a1c013df064d244503db1665707fd
hugosocie <hugosocie@gmail.com>
2018-11-30 14:28:28 +0100

    ADD : style linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-    "extends": [
-      "airbnb-base",
-      "plugin:vue/strongly-recommended"
-    ],
-    "rules": {
-      "object-curly-spacing": ["error", "always"],
-      "space-in-parens": ["error", "always"],
-      "array-bracket-spacing": ["error", "always"],
-      "no-console": "warn"
-    }
+  "extends": [
+    "airbnb-base",
+    "plugin:vue/strongly-recommended"
+  ],
+  "rules": {
+    "object-curly-spacing": ["error", "always"],
+    "space-in-parens": ["error", "always"],
+    "array-bracket-spacing": ["error", "always"],
+    "no-console": "warn"
+  }
 };
---
+++ .stylelintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": [
+    "stylelint-config-recommended",
+    "stylelint-config-standard"
+  ],
+  "rules": {
+    "max-empty-lines": 5,
+    "no-eol-whitespace": null,
+    "function-parentheses-space-inside": "always",
+    "selector-attribute-brackets-space-inside": "always",
+    "number-leading-zero": "never",
+    "string-quotes": "single"
+  }
+}
